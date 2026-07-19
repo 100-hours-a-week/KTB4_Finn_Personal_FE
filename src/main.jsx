df343e93a5bc757e0b938a61844d1bba6a3b88a6
JayJeong <80833894+JayJeong1741@users.noreply.github.com>
2026-07-19 23:39:11 +0900

    css 패키지 이동

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-import "../css/common.css";
-import "../css/home.css";
-import "../css/detail.css";
-import "../css/settings.css";
-import "../css/editor.css";
-import "../css/auth.css";
+import "./css/common.css";
+import "./css/home.css";
+import "./css/detail.css";
+import "./css/settings.css";
+import "./css/editor.css";
+import "./css/auth.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
