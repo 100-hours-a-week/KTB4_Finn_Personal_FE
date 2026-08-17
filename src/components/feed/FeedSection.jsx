34957e24ec2e8ad8e399dd8d18c1d5abf87f1dd6
JayJeong <80833894+JayJeong1741@users.noreply.github.com>
2026-08-17 23:13:49 +0900

    feat: 페이지 로딩 시 최소 로딩시간 추가

--- src/components/feed/FeedSection.jsx
+++ src/components/feed/FeedSection.jsx
@@ -19,7 +19,7 @@ function FeedSection({ posts, onTagSearch, isLoading, isLoadingMore, onLoadMore,
         }
       },
       {
-        rootMargin: "0px 0px 50px 0px",
+        rootMargin: "0px 0px 30px 0px",
       }
     );
 
